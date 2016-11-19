8f5e66ac496ecea3ed2c77535aa41a29c8cf5faa
r611171 <edward.cheadle@cambiahealth.com>
2016-11-18 20:55:07 -0700

    Started from the beginning with the script file.

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -3,37 +3,4 @@
 # Update CentOS with any patches except kernel
 yum update -y --exclude=kernel
 
-# Tools
-yum install -y nano git unzip screen
-
-# Apache 
-yum install -y httpd httpd-devel httpd-tools
-chkconfig --add httpd
-chkconfig httpd on
-service httpd stop
-
-rm -rf /var/www/html
-ln -s /vagrant /var/www/html
-
-service httpd start
-
-# PHP
-yum install -y php php-cli php-common php-devel php-mysql
-
-# MySQL  
-yum install -y mysql mysql-server  mysql-devel
-chkconfig --add  mysqld
-chkconfig mysqld on
-
-service mysqld start
-
-mysql -u root -e "SHOW DATABASES";
-
-# Download Starter content
-cd /vagrant
-sudo -u vagrant wget -q https://raw.githubusercontent.com/echeadle/vagrant/master/files/index.html
-sudo -u vagrant wget -q https://raw.githubusercontent.com/echeadle/vagrant/master/files/info.php
-
-service httpd restart
-
 
