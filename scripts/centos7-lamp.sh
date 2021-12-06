c4c6f726b8bcf06ccda340001fe079406f1062c5
echeadle <echeadle@gmail.com>
2021-12-06 13:10:45 -0700

    Update centos7=lamp file.

---
+++ scripts/centos7-lamp.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Update CentOS, except kernel, with any patches
+yum update -y --exclude=kernel
+
+# Tools
+yum install -y nano git unzip screen
+
+# Apache
+yum install -y httpd httpd-devel httpd-tools
+systemctl enable httpd
+systemctl start httpd
+
+systemctl stop httpd
+
+rm -rf /var/www/html
+ln -s /vagrant /var/www/html
+
+systemctl start httpd
+
+# PHP
+yum install -y php-cli php-common php-devel php-mysql
+
+# MySQL
+yum install -y mysql mysql-devel mariadb-server
+systemctl enable mariadb
+systemctl start mariadb
+
+
+mysql -u root -d "SHOW DATABASES";
+
+# Download Starter content
+cd /vagrant
+sudo -u vagrant wget -q https://raw.githubusercontent.com/echeadle/vagrant/master/files/index.html
+sudo -u vagrant wget -q https://raw.githubusercontent.com/echeadle/vagrant/master/files/info.php
+
+systemctl restart httpd
